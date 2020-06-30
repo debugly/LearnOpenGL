8a9874d836e9f994e6ddb0231b7d7f335323b3f3
qianlongxu <qianlongxu@sohu-inc.com>
2020-06-30 11:09:53 +0800

    红色呼吸灯

--- GLKitTutorial/GLKitTutorial/Classes/0x01/ViewController0x01.h
+++ GLKitTutorial/GLKitTutorial/Classes/0x01/ViewController0x01.h
@@ -1,9 +1,9 @@
 //
 //  ViewController0x01.h
-//  Demo01-GLKitView
+//  GLKTutorial
 //
 //  Created by Matt Reach on 2020/6/30.
-//  Copyright © 2020 Demo. All rights reserved.
+//  Copyright © 2020 GLKTutorial. All rights reserved.
 //
 // 红色屏幕
 
--- GLKitTutorial/GLKitTutorial/Classes/0x01/ViewController0x01.m
+++ GLKitTutorial/GLKitTutorial/Classes/0x01/ViewController0x01.m
@@ -1,15 +1,20 @@
 //
 //  ViewController0x01.m
-//  Demo01-GLKitView
+//  GLKTutorial
 //
 //  Created by Matt Reach on 2020/6/30.
-//  Copyright © 2020 Demo. All rights reserved.
+//  Copyright © 2020 GLKTutorial. All rights reserved.
 //
 
 #import "ViewController0x01.h"
 #import <GLKit/GLKit.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController0x01 ()<GLKViewDelegate>
+{
+    float _curRed;
+    BOOL _increasing;
+}
 
 @end
 
@@ -28,13 +33,41 @@
     glkView.delegate = self;
     //设置 glkView 的上下文
     [glkView setContext:context];
+    
+    //自主控制刷新时机
+    glkView.enableSetNeedsDisplay = NO;
+    //跟屏幕刷新频率保持一致
+    CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render:)];
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+- (void)render:(CADisplayLink *)sender
+{
+    GLKView *glkView = (GLKView *)self.view;
+    [glkView display];
 }
 
 //实现代理方法！视图需要重绘时就会调用这个方法了；
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
+    if (_increasing) {
+        _curRed += 0.01;
+    } else {
+        _curRed -= 0.01;
+    }
+    
+    if (_curRed > 1.0) {
+        _curRed = 1.0;
+        _increasing = NO;
+    }
+    
+    if (_curRed < 0) {
+        _curRed = 0.0;
+        _increasing = YES;
+    }
+    
     //设置清屏颜色
-    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClearColor(_curRed, 0.0, 0.0, 1.0);
     //执行清理
     glClear(GL_COLOR_BUFFER_BIT);
 }
